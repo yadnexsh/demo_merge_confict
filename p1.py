5a21f7aa2f8e458920cdbc50d4d973032c70fa46
yadnexsh <yadnexshu@example.com>
2025-12-23 22:14:23 +0530

    test1 1st commit

--- p1.py
+++ p1.py
@@ -1,4 +1,7 @@
 def loop():
-    for each in range(2):
+    for each in range(2000):
+        zx = random.randint(1,10)
+        y = random.randint(1,10)
+        
         x = random.randint(1,10)
     return x
